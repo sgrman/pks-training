e87a5b19f85e9a0f7dd405b3c4b2f80b64ace1c4
Nathan Ness <nathan79@gmail.com>
2018-10-20 07:33:49 -0600

    Create nsxt-setup.sh

---
+++ concourse/nsxt/nsxt-setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# EDIT names and domain
+CONCOURSE_ENDPOINT=fly.vballin.com:8080
+CONCOURSE_TARGET=nsx-concourse
+PIPELINE_NAME=install-nsx-t
+CONCOURSE_USER=nsx
+CONCOURSE_PW=vmware
+
+alias fly-s="fly -t $CONCOURSE_TARGET set-pipeline -p $PIPELINE_NAME -c nsx-t-install.yml -l nsx-t-params.yml"
+alias fly-l="fly -t $CONCOURSE_TARGET containers | grep $PIPELINE_NAME"
+alias fly-h="fly -t $CONCOURSE_TARGET hijack -b "
+
+echo "Concourse target set to $CONCOURSE_ENDPOINT"
+echo "Login using fly"
+echo ""
+
+fly --target $CONCOURSE_TARGET login -u $CONCOURSE_USER -p $CONCOURSE_PW --concourse-url http://${CONCOURSE_ENDPOINT} -n main
